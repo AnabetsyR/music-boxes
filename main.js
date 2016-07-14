b4c59d3e6b4a1462060bf252ff76a8979e76c3de
ANABETSY RIVERO <anabetsy001@gmail.com>
2016-07-14 12:26:05 -0400

    added keyboard functionality

--- main.js
+++ main.js
@@ -45,3 +45,35 @@ $("#g").click(function(){
   gNote.currentTime = 0;
   gNote.play();
 });
+
+$(document).keydown(function(e){
+  //console.log(e.keyCode);
+  if(e.keyCode == 81){
+  cNote.currentTime = 0;
+  cNote.play();
+  }
+  if(e.keyCode == 87){
+    aNote.currentTime = 0;
+    aNote.play();
+  }
+  if(e.keyCode == 69){
+    bNote.currentTime = 0;
+    bNote.play();
+  }
+  if(e.keyCode == 82){
+    dNote.currentTime = 0;
+    dNote.play();
+  }
+  if(e.keyCode == 84){
+    eNote.currentTime = 0;
+    eNote.play();
+  }
+  if(e.keyCode == 89){
+    fNote.currentTime = 0;
+    fNote.play();
+  }
+  if(e.keyCode == 85){
+    gNote.currentTime = 0;
+    gNote.play();
+  }
+});
